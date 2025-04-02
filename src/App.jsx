1de6dee8ac345924cdfe9de95d973471b786b152
Akeem Morris <akeemmorris@Akeems-Laptop.local>
2025-04-02 15:27:46 -0500

    changed the name of an import- navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import HomePage from './pages/HomePage';
 import Footer from './components/Footer';
 import './App.css';
